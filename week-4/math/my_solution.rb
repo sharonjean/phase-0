0cd7a82c8d497f88a8c5aad369b658607778bc28
Sharon <sclaitor@gmail.com>
2015-12-02 00:58:00 -0800

    Add files for address define-method and math.

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ 2 + 3
+end
+
+def subtract(num_1, num_2)
+ 2 - 3
+end
+
+def multiply(num_1, num_2)
+ 2 * 3
+end
+
+def divide(num_1, num_2)
+ 10.to_f / 2.to_f
+end
