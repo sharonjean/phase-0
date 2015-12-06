0bcc1f836977a7a930fee6a8c982ed7984659e49
Sharon <sclaitor@gmail.com>
2015-12-05 17:22:39 -0800

    Push variables-methods.rb

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,30 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(grade)
+
+if grade >= 90
+  return "A"
+end
+
+if grade >= 80
+  return "B"
+end
+
+if grade >= 70
+  return "C"
+end
+
+if grade >= 60
+  return "D"
+end
+
+if grade < 60
+  return "F"
+end
+
+end
--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,29 @@
+[Defining Math Methods](https://github.com/sharonjean/phase-0/blob/master/week-4/basic-math.rb)
+[Return a Formatted Address](https://github.com/sharonjean/phase-0/tree/master/week-4/address)
+
+
+##Reflect
+
+##How do you define a local variable?
+  '''The local variable lives within a method, and it points to whatever object you give it (string, integer, etc.)'''
+
+##How do you define a method?
+  '''A method is essentially the "what" you are telling the computer to do, the verb. You define the method by naming it, passing it an argument to process, and ending the block of code.
+  def method_name(pass in an argument)
+  end'''
+
+##What is the difference between a local variable and a method?
+  '''variables are objects that you define, and they live within the method. The method processes the function you gave it.'''
+
+##How do you run a ruby program from the command line?
+  '''you use an .rb (ruby) file to store the code, and run it by opening your file as: ruby filename.rb'''
+
+##How do you run an RSpec file from the command line?
+  '''You do the same as running a ruby file, but use: rspec filename.rb'''
+
+##What was confusing about this material? What made sense?
+  '''The rspec was difficult to get accustomed to (as far as reading), but got much better after a few tries. I would say these challenges have been simple since it is the very beginning. That may likely change very soon!'''
+
 ##Full Name Greeting
 
 puts 'What is your first name?'
