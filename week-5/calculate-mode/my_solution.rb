e67bf1303ee91eb3cb5fbefcb8f47c9cf3745476
Sharon <sclaitor@gmail.com>
2015-12-13 01:18:39 -0800

    Add calculate mode.

---
+++ week-5/calculate-mode/my_solution.rb
@@ -0,0 +1,31 @@
+# Calculate the mode Pairing Challenge
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented.
+
+
+
+# 0. Pseudocode
+
+# What is the input? Array of numbers or strings
+# What is the output? Array of most frequent value(s)
+# What are the steps needed to solve the problem?
+#
+
+# 1. Initial Solution
+
+mode = {}
+
+
+
+# 3. Refactored Solution
+
+
+
+
+# 4. Reflection
