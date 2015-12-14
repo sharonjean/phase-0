0cf4e2526fa1c2937a4b322dbe377b7aacf48f29
Sharon <sclaitor@gmail.com>
2015-12-14 01:01:03 -0800

    Add reflection.

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [2] hours on this challenge.
+# I spent [3] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -12,49 +12,80 @@
 # What is the output? Comma separated string
 # What are the steps needed to solve the problem?
 #
-#Iterate through each value and convert to string
-#Create empty container to store new values
-#Convert integer to string
-#Reverse string
-#Place a comma after every third digit if applicable
-#Reverse back
+#Define process that accepts an argument
+#Create container for values
+#Compare number size by its divisibility in thirds
+#Insert a comma after every third digit if applicable (use negative index -4)
 #Return comma separated string integer
 
 
 # 1. Initial Solution
 
-def separate_comma(integer)
-  num = ""
-  if integer.to_s.length <= 3
-    p integer.to_s
-  end
-  elsif integer%3 == 0
-    n -= 3
-    integer = integer.to_s.reverse
-  i = 0
-  while integer.length - i > 3
-    3.times do
-      num << integer[i]
-      i += 1
-  end
+# def separate_comma(number)
 
-  while i < integer.length
-    num << integer[i]
-    i += 1
-  end
-  num.reverse
-end
+#   num = number.to_s.split("")
+#   new_num = num.size/3.0
 
-
+#   if num.size < 4
+#     p number.to_s
+#   elsif num.size%3 == 0
+#     n = -4
+#     (new_num.to_i-1).times do |i|
+#       num.insert(n, ',')
+#       n -= 4
+#     end
+#     num.join("")
+#   else
+#       n = -4
+#     new_num.to_i.times do |i|
+#       num.insert(n, ',')
+#       n -= 4
+#     end
+#    num.join("")
+#   end
+# end
 
 # 2. Refactored Solution
 
+def separate_comma(number)
+  num = number.to_s.split("")
+  new_num = num.size/3.0
 
+  if num.size < 4
+    p number.to_s
+  elsif num.size%3 == 0
+    n = -4
+    (new_num.to_i-1).times do |i|
+      num.insert(n, ',')
+      n -= 4
+    end
+    num.join("")
+  else
+      n = -4
+    new_num.to_i.times do |i|
+      num.insert(n, ',')
+      n -= 4
+    end
+   num.join("")
+  end
+end
 
 
 # 3. Reflection
-#What was your process for breaking the problem down? What different approaches did you consider?
-#Was your Pseudocode effective in helping you build a successful initial solution?
-#What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them. Did it/they significantly change the way your code works? If so, how?
-#How did you initially iterate through the data structure?
-#Do you feel your refactored solution is more readable than your initial solution? Why?
+# What was your process for breaking the problem down? What different approaches did you consider?
+
+# I initially started out very basic with if elsif statements that placed comma(s) based on their placement within a range of numbers. I wasn't able to get that to run, so placed the commas with a negative index of 4. I believe reverse would be a good option here as well.
+
+# Was your Pseudocode effective in helping you build a successful initial solution?
+
+# Yes, logically it helped me with each piece.
+
+# What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them. Did it/they significantly change the way your code works? If so, how?
+
+# To be honest, I felt a bit lost with refactoring since I wasn't sure of which other method to implement. Aside from renaming and reshuffling, I had to give up due to time.
+
+# How did you initially iterate through the data structure?
+# N/A
+
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+# I really got stuck with the refactoring on this challenge.
