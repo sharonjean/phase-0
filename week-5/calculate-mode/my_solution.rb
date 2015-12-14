d3b659d03823759ae2fcba376206913eba01505d
Sharon <sclaitor@gmail.com>
2015-12-13 22:24:34 -0800

    Add calculate mode.

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -2,7 +2,7 @@
 
 # I worked on this challenge [by myself, with: Roche Janken]
 
-# I spent [] hours on this challenge.
+# I spent [2] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -30,45 +30,58 @@
 
 # 1. Initial Solution
 
-def mode(init_array)
-  #new_hash=Hash.new{|h, k| h[k]=[]}
-  new_hash={}
-  array_to_return=[]
-  init_array.sort!
-  # print init_array
-  init_array.each do |item|
-    new_hash[item] = init_array.count(item)
-  end
-  really_new_hash = new_hash.sort_by { |key,value| value }.reverse.first
+# def mode(init_array)
+#   #new_hash=Hash.new{|h, k| h[k]=[]}
+#   new_hash={}
+#   array_to_return=[]
+#   init_array.sort!
+#   # print init_array
+#   init_array.each do |item|
+#     new_hash[item] = init_array.count(item)
+#   end
+#   really_new_hash = new_hash.sort_by { |key,value| value }.reverse.first
 
-  new_hash.each do |item|
-    if new_hash[item]==really_new_hash[1]
-      # push key to array_to_return
-      array_to_return << key
-    else
-      nil
-    end
-  end
+#   new_hash.each do |item|
+#     if new_hash[item]==really_new_hash[1]
+#       # push key to array_to_return
+#       array_to_return << key
+#     else
+#       nil
+#     end
+#   end
 
-  print really_new_hash
-  print array_to_return
+#   print really_new_hash
+#   print array_to_return
 
-end
+# end
 
 #hash={1=>[1,1,1], 2 => {2,2}}
-#hash[1].length > hash[2].lengh
+#hash[1].length > hash[2].length
 
-#DRIVER CODE
+#3 Refactored Solution
 
-
-mode([1,2,1,3,4,4,4,5,3,2])
-
-
-# 3. Refactored Solution
-
-
-# 3. Refactored Solution
-
-
+def mode(container)
+  frequency = []
+  hash = Hash.new(0)
+  max_v = container.values.max
+  container.each do |n|
+    hash[n] += 1
+  end
+  hash.each do |k, v|
+    if v == max_v
+      frequency << k
+    end
+  end
+  frequency
+end
 
 # 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+Both an array and a hash. We wanted to store the values in an array to be easier to return.
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+I'd say about the same degree of difficulty. Part of it is not knowing exactly which methods to implement, which would determine how to structure the code. I will say that having the pseudocode is tremendously helpful in identifying where you are in your code, and as a guide.
+# What issues/successes did you run into when translating your pseudocode to code?
+  Again, not knowing which method to implement means the pseudocode could potentially not match the final code. However, it's still helpful in terms of being able to create some sort of skeleton to work from.
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+  tbc
