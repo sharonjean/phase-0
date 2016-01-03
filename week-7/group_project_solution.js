2dd2d76ade4b71628417d7712509e35b75d90f5b
Sharon <sclaitor@gmail.com>
2016-01-03 01:18:39 -0800

    Update file to exclude User Stories.

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,9 +1,4 @@
-##USER STORIES
 
-// /As a user i’d like to do three things to a list of numbers.
-// Using “sum” and the list I want to add together all the numbers whether the list is even or odd in length.
-// I’d like to use “mean” to return the average of all the numbers in the list.
-// Finally I’d like to use “median” to return the median number of the list./
 
 ##PSEUDOCODE
 
