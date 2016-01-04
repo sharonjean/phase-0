b4ed4ecb4fcd6393790654898bd38c95e889e738
Sharon <sclaitor@gmail.com>
2016-01-04 08:28:34 -0800

    Add release 4 to group project

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,26 +1,81 @@
+## USER STORIES
+// /As a user i’d like to do three things to a list of numbers.
+// Using “sum” and the list I want to add together all the numbers whether the list is even or odd in length.
+// I’d like to use “mean” to return the average of all the numbers in the list.
+// Finally I’d like to use “median” to return the median number of the list./
 
+## PSEUDOCODE
 
-##PSEUDOCODE
+// input: An array of numbers
+// output: 1) the sum of all the numbers inside the array, 2) the "mean" average of the numbers inside the array, 3) the median number from the array
 
-input: An array of numbers
-output: 1) the sum of all the numbers inside the array, 2) the "mean" average of the numbers inside the array, 3) the median number from the array
+// # Create an array of single-digit numbers
+// # Iterate through the array of numbers to calculate and return its sum (non-destructively)
 
-# Create an array of single-digit numbers
-# Iterate through the array of numbers to calculate and return its sum (non-destructively)
+// # Define a method that accepts the array as an argument and returns the "mean" average
+//   # Divide the sum of the array by the    amount of numbers inside the array
+//   # Return the "mean average" number
 
-# Define a method that accepts the array as an argument and returns the "mean" average
-  # Divide the sum of the array by the    amount of numbers inside the array
-  # Return the "mean average" number
+// # Define a method that accepts the array as an argument and returns the "median" number
+//   # Sort the array in ascending order
+//     # IF array length odd?,
+//       # Subtract the array(by length) by 1, and divide by 2
+//       # return median value
+//     # ELSE if array length even?,
+//       # Take array(by length) and divide by 2,
+//       # add this value to the value of
+//       # The array(by length), subtracted by 1, divided by 2
+//       # Take the sum of these index values and divide by 2
+//       # return median value
 
-# Define a method that accepts the array as an argument and returns the "median" number
-  # Sort the array in ascending order
-    # IF array length odd?,
-      # Subtract the array(by length) by 1, and divide by 2
-      # return median value
-    # ELSE if array length even?,
-      # Take array(by length) and divide by 2,
-      # add this value to the value of
-      # The array(by length), subtracted by 1, divided by 2
-      # Take the sum of these index values and divide by 2
-      # return median value
+## CODE
 
+var numArray = [7,7,7,7,1,2];
+
+function sum(array) {
+  var indexes = array.length;
+  var sum = 0
+  for (var number = 0; number < indexes; number ++)
+    sum += array[number];
+  return sum
+}
+
+function mean(array) {
+  var mean = (sum(array))/(array.length)
+  return mean
+}
+
+function median(array) {
+  array.sort( function(a,b) {return a - b;} );
+  var half = Math.floor(array.length/2);
+  if ((array.length % 2) != 0)
+    return (array[half-1] + array[half]) / 2.0;
+  else
+    return array[half]
+}
+
+## REFACTORED CODE
+
+var numArray = [7,7,7,7,1,2];
+//As a user I want to create a list of numbers
+
+//refactored using reduce method
+var sum = numArray.reduce(function(a, b){return a+b;})
+console.log(sum);
+//As a user I want to add the sum of all the numbers within the list
+
+function mean(array) {
+  var mean = (sum(array))/(array.length)
+  return mean
+}
+//As a user I want to find the mean value of the list of numbers
+
+function median(array) {
+  array.sort( function(a,b) {return a - b;} );
+  var half = Math.floor(array.length/2);
+  if ((array.length % 2) != 0)
+    return (array[half-1] + array[half]) / 2.0;
+  else
+    return array[half]
+}
+//As a user I want to find the median from the list of numbers
---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,72 @@
+ // JavaScript Olympics
+
+// I paired [by myself, with: Gino] on this challenge.
+
+// This challenge took me [#] hours.
+
+
+
+// Bulk Up
+
+// Pseudocode
+// input: function that takes an array of athletes with event and win property
+// output: print name of winner and event
+// Create function with argument of array
+// Create property that shows event place
+// Create method that declares winner
+
+// athletesArray = ["Michael", "Alice", "Jerry", "Gino", "Sharon"]
+
+// function bulkUp(winner, event) {
+//   this.winner = winner;
+//   this.event = event;
+// }
+
+// var athlete = {
+//  event: "Bobsled"
+//  winner: "Bob Jones"
+// }
+
+// var athlete1 = new bulkUp(winner,event)
+// function win() {
+//   console.log(athlete1.winner + " won the " + athlete1.event)
+// }
+
+
+// }
+
+
+
+// Jumble your words
+// Input: A function that accepts a string as an argument
+// Output: The string reversed
+// Created a method that take a string and use built in methods to reverse it
+
+// function reverseString(string) {
+//  return string.split('').reverse().join('');
+// }
+// console.log(reverseString("Gino"));
+
+
+// 2,4,6,8
+
+// Pseudocode
+// input: Function that accepts array of numbers
+// output: Array of even numbered numbers
+// Create object that accepts an array of numbers
+// While the number in an array is divisible by 2, return array of even numbers
+
+var num = [1,2,3,4,5,6,7,8,9,10]
+
+function evenArray(num) {
+
+ return num.every(a[a.even?]);
+}
+  console.log(num);
+
+// "We built this city"
+
+
+
+
+// Reflection
