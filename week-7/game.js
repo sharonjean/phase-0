7b114cbdc0398b161c3de995d69c78a011b595b3
Sharon <sclaitor@gmail.com>
2016-01-05 02:23:44 -0800

    Add basic game file.

---
+++ week-7/game.js
@@ -0,0 +1,41 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: Create madlibs game!
+// Goals: Prompt user for input for various nouns, adjectives, and names. Set up alert that shows small bits of storyline with user input inserted.
+// Characters: N/A
+// Objects: Story
+// Functions: Prompt user for input, alert user with storyline.
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
