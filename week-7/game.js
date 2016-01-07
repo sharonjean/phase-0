47ff4ef345faf3fcf2dc2ab2c57f460df3719b96
Sharon <sclaitor@gmail.com>
2016-01-06 21:25:43 -0800

    Add reflection to solo challenge

--- week-7/game.js
+++ week-7/game.js
@@ -4,38 +4,46 @@
 
 // Your mission description:
 // Overall mission: Create madlibs game!
-// Goals: Prompt user for input for various nouns, adjectives, and names. Set up alert that shows small bits of storyline with user input inserted.
-// Characters: N/A
-// Objects: Story
-// Functions: Prompt user for input, alert user with storyline.
+// Goals: Prompt user for input for various nouns, adjectives, and names. Insert selected keywords into story-line, and print completed script.
+// Characters: NA
+// Objects: Field boxes, Story
+// Functions: Prompt user for input, return with storyline
 
 // Pseudocode
-//
-//
-//
-//
-//
+// Create html for content on landing page, and outline
+// Stylize content with css stylesheet
+// Write methods to prompt user for input,
+// and to insert selected keywords into storyline
+// Return completed mad lib!
 
 // Initial Code
 
+function madLibs() {
 
+    var storyDiv = document.getElementById("story");
 
+    var name = document.getElementById("name").value;
 
+    var adjective = document.getElementById("adjective").value;
 
+    var noun = document.getElementById("noun").value;
 
-// Refactored Code
+    storyDiv.innerHTML = name + " has a " + adjective + " " + noun + " and can't make it to the party!";
 
+    }
 
-
-
-
+var libButton = document.getElementById("button");
+libButton.addEventListener("click", madLibs);
 
 // Reflection
-//
-//
-//
-//
-//
-//
-//
-//
+// What was the most difficult part of this challenge?
+/*Figuring out during the Pseudocode process exactly which functions you be required to make this task smooth. My first inclination was to make a word jumble game, but I quickly found out there were far more components to making that work than I had intended, so I decided to go with something simpler.*/
+
+// What did you learn about creating objects and functions that interact with one another?
+// It just solidified the idea of creating these objects, to create methods you will perform by incoporating the objects.
+
+// Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+// You can see by the code that the document.getElementById was used heavily! This gives the property an "id" that you can later access in methods/functions to perform tasks you'd like them to do.
+
+// How can you access and manipulate properties of objects?
+// By creating a function and using  getElementById("example property") to access the properties, see above!
